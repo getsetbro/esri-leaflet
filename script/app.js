a1e45bf6a7e2555df138ec82e945860d8feb3ab1
set bro <sbro@hotmail.com>
2019-02-13 11:18:33 -0500

    types def

---
+++ script/app.js
@@ -0,0 +1,37 @@
+var uri = "https://services.arcgis.com/rOo16HdIMeOBI4Mb/ArcGIS/rest/services/USA_Rail_Network/FeatureServer/0";
+function runr(lat, lon) {
+    var map1 = L.map("map").setView([lat, lon], 5);
+    var map = L.map("map").setView([45.526, -122.667], 9);
+    // @ts-ignore
+    L.esri.basemapLayer("Gray").addTo(map);
+    // var items = L.esri.featureLayer({ url: uri }).addTo(map);
+    // const worldCities = L.esri
+    //   .featureLayer({
+    //     url:
+    //       "https://sampleserver6.arcgisonline.com/arcgis/rest/services/SampleWorldCities/MapServer/0"
+    //   })
+    //   .addTo(map);
+    // worldCities.bindPopup(function(e) {
+    //   return L.Util.template(
+    //     "<div>{CITY_NAME}</div><div>" +
+    //       e.feature.properties.POP.toLocaleString() +
+    //       "</div>",
+    //     e.feature.properties
+    //   );
+    // });
+    // @ts-ignore
+    var qwer = L.esri.Cluster.featureLayer({
+        url: "https://services.arcgis.com/rOo16HdIMeOBI4Mb/arcgis/rest/services/Trimet_Transit_Stops/FeatureServer/0"
+    }).addTo(map);
+    qwer.bindPopup(function (e) {
+        var zxcv = JSON.stringify(e.feature.properties)
+            .split(",")
+            .join("</div><div>");
+        return L.Util.template("<div>" + zxcv + "</div>", e.feature.properties);
+    });
+}
+if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+        runr(position.coords.latitude, position.coords.longitude);
+    });
+}
